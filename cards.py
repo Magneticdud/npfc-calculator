3b20fc0faf54be7f57f799bfd41a4d3b260252ee
Magneticdud <Magneticdud@users.noreply.github.com>
2024-09-10 14:49:41 +0200

    Create cards.py

---
+++ cards.py
@@ -0,0 +1,11 @@
+cards = {
+  #Tattica
+  'Video analisi': {'Tiro': 1, 'Velocità': 0, 'Resistenza': 0, 'Tecnica': 0, 'Forza': 0, 'Elevazione':0, 'Determinazione':1},
+  'Marcatura': {'Tiro': 0, 'Velocità': 0, 'Resistenza': 2, 'Tecnica': 0, 'Forza': 2, 'Elevazione':0, 'Determinazione':0},
+  'Pressing': {'Tiro': 0, 'Velocità': 2, 'Resistenza': 0, 'Tecnica': 1, 'Forza': 0, 'Elevazione':0, 'Determinazione':0},
+  'Partitella': {'Tiro': 1, 'Velocità': 0, 'Resistenza': 0, 'Tecnica': 1, 'Forza': 0, 'Elevazione':2, 'Determinazione':0},
+
+  #Tecnica
+  'Tiri': {'Tiro': 4, 'Velocità': 0, 'Resistenza': 1, 'Tecnica': 0, 'Forza': 0, 'Elevazione':0, 'Determinazione':0},
+  'Passaggi': {'Tiro': 0, 'Velocità': 3, 'Resistenza': 0, 'Tecnica': 3, 'Forza': 0, 'Elevazione':0, 'Determinazione':0},
+}
